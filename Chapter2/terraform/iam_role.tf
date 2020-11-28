7adfc2e5620b62cdf7c945dfe182f315ac532f77
Ubuntu <ubuntu@ip-172-31-35-88.us-west-2.compute.internal>
2020-11-28 02:33:13 +0000

    modifying terraform code to version 0.12 compatible

--- Chapter2/terraform/iam_role.tf
+++ Chapter2/terraform/iam_role.tf
@@ -1,61 +1,63 @@
 provider "aws" {
   region = "us-west-2"
 }
-resource "aws_iam_role" "test_iam_role" {
-  name = "test_Iam_role"
+
+
+resource "aws_iam_role_policy" "my-test-policy" {
+  name = "my-test-iam-policy"
+  role = "${aws_iam_role.my-test-iam-role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket", 
+                "s3:PutObject", 
+                "s3:GetObject" 
+		],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "my-test-iam-role" {
+  name = "my-test-iam-role"
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+"Version": "2012-10-17",
+"Statement": [
+{
+"Action": "sts:AssumeRole",
+"Principal": {
+ "Service": "ec2.amazonaws.com"
+},
+"Effect": "Allow"
+}
+]
 }
 EOF
 
   tags = {
-    Name = "test-iam-role"
+    tag-key = "my-test-iam-role"
   }
 }
 
-resource "aws_iam_instance_profile" "test_iam_profile" {
-  name = "test_iam_profile"
-  role = "aws_iam_role.test_iam_role.name"
+resource "aws_iam_instance_profile" "my-test-iam-instance-profile" {
+  name = "my-test-iam-instance-profile"
+  role = "${aws_iam_role.my-test-iam-role.name}"
 }
 
-resource "aws_iam_role_policy" "test_iam_policy" {
-  name = "test_iam_policy"
-  role = "aws_iam_role.test_iam_role.id"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:ListBucket",
-        "s3:PutObject",
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
 
 resource "aws_instance" "test_ec2_role" {
   ami                  = "ami-0d5fad86866a3a449"
   instance_type        = "t2.micro"
-  iam_instance_profile = "aws_iam_instance_profile.test_iam_profile.name"
-  key_name             = "packtpub"
+  iam_instance_profile = "${aws_iam_instance_profile.my-test-iam-instance-profile.name}"
+  key_name	       = "packtpub"	
 }
-
