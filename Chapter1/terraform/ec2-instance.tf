e45131e763f34749aac7c639687aaf11ec1c11b5
plakhera <laprashant@gmail.com>
2020-11-28 00:37:00 +0000

    ading provider block and removing whitespaces

--- Chapter1/terraform/ec2-instance.tf
+++ Chapter1/terraform/ec2-instance.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "test_instance" { 
+provider "aws" {
+  region = "us-west-2"
+}
 
-     ami = "ami-0bc06212a56393ee1" 
-
-     instance_type = "t2.micro" 
-
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0bc06212a56393ee1"
+  instance_type = "t2.micro"
 }
